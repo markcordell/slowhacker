be0b0992a4abd3fd7573cc501609eb2860ace400
Mark Cordell <mark.allen.cordell@gmail.com>
2019-06-29 20:58:13 -0700

    updating encoding and fixes lambda function bug

--- src/main-updater/handler.py
+++ src/main-updater/handler.py
@@ -18,7 +18,7 @@ def get_articles(timestamp: str):
     r = requests.get("{url}?query=&hitsPerPage=10&minWordSizefor1Typo=4&minWordSizefor2Typos=8&advancedSyntax=true&ignorePlurals=false&tagFilters=%5B%22story%22%5D&numericFilters=%5B%22created_at_i%3E{seconds}%22%5D&page=0&queryType=prefixLast&typoTolerance=true&restrictSearchableAttributes=%5B%5D".format(url=url, seconds=timestamp))
     return r.text
 
-def last_day():
+def last_days_article():
     now = datetime.datetime.now()
     last_time = now - datetime.timedelta(days=days)
     seconds = (now-last_time).total_seconds()
@@ -26,17 +26,21 @@ def last_day():
     articles = json.loads(get_articles(unix_date))
     return articles["hits"]
 
+def parse_articles(articles):
+    data = []
+    for i in articles:
+        comment = "{url}/item?id={id}".format(url=hacker_news_url, id=i["objectID"])
+        data.append([i["url"],i["title"],comment])
+    return data
+
+
 def main():
     s3 = boto3.resource('s3')
     env = Environment(loader=FileSystemLoader('./templates'))
-    last = last_day()
-    data = []
-    for i in last:
-        comment = "{url}/item?id={id}".format(url=hacker_news_url, id=i["objectID"])
-        data.append([i["url"],i["title"],comment])
+    articles = last_days_article()
+    data = parse_articles(articles)
     template = env.get_template('s3-index.html')
     body = template.render(data=data, timestamp=datetime.datetime.now().strftime("%m-%d-%Y %I:%M %p"))
-    body
 
     s3.Bucket(bucket).put_object(Key='index.html', Body=body, ContentType='text/html')
 
