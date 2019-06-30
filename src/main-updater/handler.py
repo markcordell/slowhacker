4849990b7c2f9b844cde0262193b86aaded50311
Mark Cordell <mark.allen.cordell@gmail.com>
2019-06-29 22:03:32 -0700

    shorttens url, and re-names template

--- src/main-updater/handler.py
+++ src/main-updater/handler.py
@@ -9,13 +9,15 @@ from botocore.vendored import requests
 from jinja2 import Environment, FileSystemLoader, select_autoescape, Template
 
 days = 1
+number_of_articles = 10
 url = "https://hn.algolia.com/api/v1/search"
 hacker_news_url = "https://news.ycombinator.com"
 bucket = os.environ['S3_BUCKET']
+styles = 'styles.css'
 
 
 def get_articles(timestamp: str):
-    r = requests.get("{url}?query=&hitsPerPage=10&minWordSizefor1Typo=4&minWordSizefor2Typos=8&advancedSyntax=true&ignorePlurals=false&tagFilters=%5B%22story%22%5D&numericFilters=%5B%22created_at_i%3E{seconds}%22%5D&page=0&queryType=prefixLast&typoTolerance=true&restrictSearchableAttributes=%5B%5D".format(url=url, seconds=timestamp))
+    r = requests.get("{url}?query=&hitsPerPage={number_of_articles}&tagFilters=%5B%22story%22%5D&numericFilters=%5B%22created_at_i%3E{seconds}%22%5D&queryType=prefixLast".format(url=url, seconds=timestamp, number_of_articles=number_of_articles))
     return r.text
 
 def last_days_article():
@@ -39,10 +41,13 @@ def main():
     env = Environment(loader=FileSystemLoader('./templates'))
     articles = last_days_article()
     data = parse_articles(articles)
-    template = env.get_template('s3-index.html')
-    body = template.render(data=data, timestamp=datetime.datetime.now().strftime("%m-%d-%Y %I:%M %p"))
+    template = env.get_template('index.html')
+    content = template.render(data=data, timestamp=datetime.datetime.now().strftime("%m-%d-%Y %I:%M %p"), styles=styles)
+    archive_content = template.render(data=data, timestamp=datetime.datetime.now().strftime("%m-%d-%Y %I:%M %p"), styles="../../{styles}".format(styles=styles))
+    date = datetime.datetime.now().strftime("%m-%d-%Y")
 
-    s3.Bucket(bucket).put_object(Key='index.html', Body=body, ContentType='text/html')
+    s3.Bucket(bucket).put_object(Key='index.html', Body=content, ContentType='text/html')
+    s3.Bucket(bucket).put_object(Key="archives/{date}/index.html".format(date=date), Body=archive_content, ContentType='text/html')
 
 if __name__ == "__main__":
     main()
